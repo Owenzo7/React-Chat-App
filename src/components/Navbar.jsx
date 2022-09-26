221ab3b4a6d3b58acf49a6c9ad90a435d4152a89
Owen Lee <owenlee545@gmail.com>
2022-09-26 16:54:50 +0300

    Just added the Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+const style = {
+
+nav:``,
+heading:``
+
+}
+
+function Navbar() {
+  return (
+    <div className={style.nav}>
+        
+       <h1 className={style.heading}>
+        Chat App
+       </h1>
+        
+        
+        </div>
+  )
+}
+
+export default Navbar
